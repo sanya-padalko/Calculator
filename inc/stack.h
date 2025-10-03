9146104452e3271b497d031c1358762671593e10
sanya-padalko <apadalko90@gmail.com>
2025-10-03 21:31:28 +0300

    Константные расширители

--- inc/stack.h
+++ inc/stack.h
@@ -2,25 +2,70 @@
 #define _STACK_H_
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
 #include "errors.h"
-#include "verify.h"
+#include "vars.h"
+
+typedef int StackElem_t;
+
+struct stack_t {
+    StackElem_t* data = NULL;
+
+    unsigned long hash = 0;
+
+    ssize_t size      = 0;
+    ssize_t capacity  = 0;
+};
+
+const StackElem_t POIZON_VALUE = 146988;
+const StackElem_t CANARY_LEFT  = 52954; // CEDA
+const StackElem_t CANARY_RIGHT = 65242; // FEDA
 
 #ifdef DEBUG
 #define make_stack(name, capacity) \
-        stack_t* name = StackCtor(capacity + 2, VarInfoCtor(#name, __FILE__, __FUNCTION__, __LINE__));
+        stack_t* name = StackCtor(capacity, VarInfoCtor(#name, __FILE__, __FUNCTION__, __LINE__));
 #define ON_DEBUG(...) __VA_ARGS__
 #else
 #define make_stack(name, capacity) \
-        stack_t* name = StackCtor(capacity + 2);
+        stack_t* name = StackCtor(capacity);
 #define ON_DEBUG(...)
 #endif
+
+#define stackdump(name) StackDump(name, VarInfoCtor(#name, __FILE__, __FUNCTION__, __LINE__));
+
+#define stackverify(stack)  StackErr_t code_error = StackVerify(stack); \
+                            if (code_error != NOTHING) { \
+                                ON_DEBUG(stackdump(stack)); \
+                                ParseErr(code_error); \
+                            }
+
+const long mod = 998244353;
+
+const ssize_t MaxCapacity = 1000000;
+const int MaxPrintedCount = 15;
+
+const int ExpandMn =  2;
+const int CheckMn  =  4;
+const int NarrowMn =  3;
+const int BadSize  = -1;
+
+unsigned long calc_hash(stack_t *stack);
+
+void fill_poizon(stack_t *stack, int left, int right);
 
 stack_t* StackCtor(ssize_t capacity ON_DEBUG(, VarInfo varinfo));
 
 StackErr_t StackDtor(stack_t *stack);
 
+static int StackRealloc(stack_t *stack, ssize_t new_size);
+
 StackErr_t StackPush(stack_t *stack, StackElem_t new_value);
 
 StackElem_t StackPop(stack_t *stack);
+
+StackErr_t StackVerify(stack_t* stack);
+
+void StackDump(stack_t *stack, VarInfo varinfo);
 
 #endif
--- src/stack.cpp
+++ src/stack.cpp
@@ -1,34 +1,40 @@
 #include "stack.h"
 
+unsigned long calc_hash(stack_t *stack) {
+    unsigned long hash = 0;
+    for (int i = 0; i < stack->capacity; ++i)
+        hash = (hash + (i + 1) * stack->data[i]) % mod;
+    
+    return hash;
+}
+
+void fill_poizon(stack_t *stack, int left, int right) {
+    for (int i = left; i < right; ++i) {
+        stack->data[i] = POIZON_VALUE;
+    }
+}
+
 stack_t* StackCtor(ssize_t capacity ON_DEBUG(, VarInfo varinfo)) {
     ON_DEBUG(PrintVarInfo(varinfo));
 
     stack_t *stack = (stack_t*)calloc(1, sizeof(stack_t));
 
-    if (stack == NULL) {
-        printerr(RED_COLOR "Calloc return NULL (during creating struct) in Stack's Constuctor\n" RESET_COLOR);
-        return NULL;
-    }
+    my_assert(stack, NULLPTR, NULL);
 
-    if (capacity > 10000000 || capacity < 2) {      // magic number
-        printerr(RED_COLOR "Unavailable size of stack in Stack's Constructor\n" RESET_COLOR);
-        return NULL;
-    }
+    my_assert(0 <= capacity && capacity <= MaxCapacity, CAPACITY_ERR, NULL);
 
-    stack->size = 2;        // 0
+    stack->size = 0;
     stack->capacity = capacity;
 
-    StackElem_t* stack->data = (StackElem_t*)calloc(stack->capacity, sizeof(StackElem_t));
-    if (stack->data == NULL) {
-        printerr(RED_COLOR "Calloc return NULL in Stack Constructor\n" RESET_COLOR);
-        return NULL;
-    }
+    stack->data = (StackElem_t*)calloc(stack->capacity + 2, sizeof(StackElem_t));
+    
+    my_assert(stack->data, NULLPTR, NULL);
 
     stack->data[0] = CANARY_LEFT;
-    stack->data[stack->capacity - 1] = CANARY_RIGHT;
+    ++stack->data;
+    stack->data[stack->capacity] = CANARY_RIGHT;
 
-    for (size_t i = 1; i < stack->capacity - 1; ++i)
-        stack->data[i] = POIZON_VALUE;
+    fill_poizon(stack, 0, stack->capacity);
 
     stack->hash = calc_hash(stack);
 
@@ -37,8 +43,7 @@ stack_t* StackCtor(ssize_t capacity ON_DEBUG(, VarInfo varinfo)) {
 
 StackErr_t StackDtor(stack_t *stack) {
     stackverify(stack);
-    if (code_error != NOTHING)
-        return code_error;
+    my_assert(code_error == NOTHING, code_error, code_error);
 
     free(stack->data);
     stack->data = NULL;
@@ -50,34 +55,22 @@ StackErr_t StackDtor(stack_t *stack) {
 
 static int StackRealloc(stack_t *stack, ssize_t new_size) {
     stackverify(stack);
-    if (code_error != NOTHING)
-        return -1;  // magic number
+    my_assert(code_error == NOTHING, code_error, BadSize);
 
-    if (new_size < stack->size) {
-        printerr(RED_COLOR "Unavailable realloc-size of stack\n" RESET_COLOR);
-        return -1;
-    }
+    my_assert(new_size >= stack->size, SIZE_ERR, BadSize);
 
-    void* realloc_ptr = realloc(stack->data, new_size * sizeof(StackElem_t));
-    if (realloc_ptr == NULL) {
-        ON_DEBUG(stackdump(stack));
-        ParseErr(REALLOC_ERR);
-        return -1;
-    }
+    --stack->data;
+    stack->data = (StackElem_t*)realloc(stack->data, (new_size + 2) * sizeof(StackElem_t));
 
-    stack->data = (StackElem_t*)realloc_ptr;
+    my_assert(stack->data, REALLOC_ERR, BadSize);
 
-    stack->data[stack->capacity - 1] = POIZON_VALUE;
+    stack->data[0] = CANARY_LEFT;
+    ++stack->data;
+    stack->capacity = new_size;
 
-    while (stack->capacity < new_size) {
-        ++stack->capacity;
-        stack->data[stack->capacity - 1] = POIZON_VALUE;
-    }
-
-    while (stack->capacity > new_size)
-        --stack->capacity;
-
-    stack->data[stack->capacity - 1] = CANARY_RIGHT;
+    fill_poizon(stack, stack->size, stack->capacity);
+    
+    stack->data[stack->capacity] = CANARY_RIGHT;
 
     stack->hash = calc_hash(stack);
 
@@ -86,33 +79,27 @@ static int StackRealloc(stack_t *stack, ssize_t new_size) {
 
 StackErr_t StackPush(stack_t *stack, StackElem_t new_value) {
     stackverify(stack);
-    if (code_error != NOTHING)
-        return code_error;
+    my_assert(code_error == NOTHING, code_error, code_error);
 
     if (stack->size == stack->capacity) {
-        int new_capacity = StackRealloc(stack, stack->size * 2);            // magic number
+        int new_capacity = StackRealloc(stack, stack->size * ExpandMn);
 
-        if (new_capacity == -1) {
-            printerr(RED_COLOR "Push was not completed\n" RESET_COLOR);
-            return REALLOC_ERR;
-        }
+        my_assert(new_capacity != BadSize, REALLOC_ERR, REALLOC_ERR);
     }
 
-    *(stack->data + stack->size - 1) = new_value;
+    *(stack->data + stack->size) = new_value;
+    ++stack->size;
     
     stack->hash = calc_hash(stack);
-
-    ++stack->size;
 
     return NOTHING;
 }
 
 StackElem_t StackPop(stack_t *stack) {
     stackverify(stack);
-    if (code_error != NOTHING)
-        return POIZON_VALUE;
+    my_assert(code_error == NOTHING, code_error, POIZON_VALUE);
 
-    if (stack->size == 2) {
+    if (stack->size == 0) {
         ON_DEBUG(stackdump(stack));
         ParseErr(EMPTY_STACK);
         printerr(RED_COLOR "You cannot pop from empty stack\n\n" RESET_COLOR);
@@ -120,17 +107,121 @@ StackElem_t StackPop(stack_t *stack) {
     }
 
     --stack->size;
-    StackElem_t last = *(stack->data + stack->size - 1);
-    *(stack->data + stack->size - 1) = POIZON_VALUE;
+    StackElem_t last = *(stack->data + stack->size);
+    *(stack->data + stack->size) = POIZON_VALUE;
 
     stack->hash = calc_hash(stack);
-
+    
     int new_capacity = stack->capacity;
-    if (stack->size * 4 == stack->capacity)
-        new_capacity = StackRealloc(stack, stack->size);
+    if (stack->size * CheckMn == stack->capacity)
+        new_capacity = StackRealloc(stack, stack->capacity / NarrowMn);
 
-    if (new_capacity == -1)
+    stack->hash = calc_hash(stack);
+    
+    if (new_capacity == BadSize)
         return POIZON_VALUE;
 
     return last;
+}
+
+StackErr_t StackVerify(stack_t* stack) {
+    if (!stack) 
+        return NULLPTR;
+
+    if (stack->size < 0)
+        return SIZE_ERR;
+    
+    if (stack->capacity < 0)
+        return CAPACITY_ERR;
+
+    if (stack->size > stack->capacity)
+        return CAP_SIZE_ERR;
+
+    if (stack->data == NULL)
+        return NULLPTR;
+
+    if (*(stack->data - 1) != CANARY_LEFT || stack->data[stack->capacity] != CANARY_RIGHT)
+        return CANARY_ERR;
+
+    if (calc_hash(stack) != stack->hash)
+        return HASH_ERR;
+
+    return NOTHING;
+}
+
+void StackDump(stack_t* stack, VarInfo varinfo) {
+    printerr("\nStackDump called from %s: in function %s, line %d\n", varinfo.file_name, varinfo.func_name, varinfo.line_ind);
+    printerr("%s ", varinfo.stack_name);
+
+    printerr("[");
+    if (stack == NULL) {
+        printerr(RED_COLOR "NULL" RESET_COLOR);
+        printerr("]");
+        printerr(RED_COLOR "(!!!! BAD !!!!)\n" RESET_COLOR);
+    }
+    else {
+        printerr(GREEN_COLOR "%x" RESET_COLOR, stack);
+        printerr("]\n");
+    }
+
+    printerr("\t{\n");
+
+    if (stack) {
+        if (stack->size > stack->capacity || stack->size < 0) {
+            printerr(RED_COLOR "\tsize = %d    (!!! BAD !!!)\n" RESET_COLOR, stack->size); 
+        }
+        else {
+            printerr(GREEN_COLOR "\tsize = %d\n" RESET_COLOR, stack->size);
+        }
+
+        if (stack->size > stack->capacity || stack->capacity < 0) {
+            printerr(RED_COLOR "\tcapacity = %d (!!! BAD !!!)\n" RESET_COLOR, stack->capacity);
+        }
+        else {
+            printerr(GREEN_COLOR "\tcapacity = %d\n" RESET_COLOR, stack->capacity);
+        }
+
+        printerr(BLUE_COLOR "\tHash = %d\n" RESET_COLOR, stack->hash);
+        
+        printerr("\tdata [");
+        if (stack->data == NULL) {
+            printerr(RED_COLOR "NULL" RESET_COLOR);
+            printerr("]");
+            printerr(RED_COLOR "(!!!! BAD !!!!)\n" RESET_COLOR);
+            
+            printerr("\t\t{\n");
+        }
+        else {
+            printerr(GREEN_COLOR "%x" RESET_COLOR, stack->data);
+            printerr("]\n");
+            
+            printerr("\t\t{\n");
+
+            if (stack->capacity >= 0) {
+                printerr((*(stack->data - 1) == CANARY_LEFT) ? GREEN_COLOR : RED_COLOR);
+                printerr("\t\t   [%2d] = %10d (CANARY)\n" RESET_COLOR, -1, *(stack->data - 1));
+
+                for (ssize_t i = 0; i < stack->capacity; ++i) {
+                    if (i >= MaxPrintedCount) {
+                        printerr("\t\t\t...\n\n");
+                        break;
+                    }
+
+                    if (*(stack->data + i) == POIZON_VALUE) {
+                        printerr("\t\t   [%2d] = %10d (POIZON)\n", i, POIZON_VALUE);
+                    }
+                    else {
+                        printerr("\t\t * [%2d] = %10d\n", i, *(stack->data + i));
+                    }
+                }
+                
+                printerr((stack->data[stack->capacity] == CANARY_RIGHT) ? GREEN_COLOR : RED_COLOR);
+                printerr("\t\t   [%2d] = %10d (CANARY)\n" RESET_COLOR, stack->capacity, stack->data[stack->capacity]);
+            }
+        }
+
+        printerr("\t\t}\n");
+    }
+
+    printerr("\t}\n");
 }
