9e1b5c5fcb0df2dad3f78a4810279dcd96196245
sanya-padalko <apadalko90@gmail.com>
2025-10-08 23:53:19 +0300

    Также assertы

--- src/vars.cpp
+++ src/vars.cpp
@@ -11,6 +11,6 @@ VarInfo VarInfoCtor(const char* object_name, const char* file_name, const char* 
     return varinfo;
 }
 
-void PrintVarInfo(const VarInfo varinfo) {
+CodeError_t PrintVarInfo(const VarInfo varinfo) {
     printf("\"%s\" %s, %s: %d\n", varinfo.object_name, varinfo.file_name, varinfo.func_name, varinfo.line_ind);
 }
